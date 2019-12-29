49ffa8ea1ffa34bb9c2a85e2669d7a4b563940ad
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-12-29 22:42:05 +0900

    feat: 予算テーブルを作成

--- app/controllers/budget_groups_controller.rb
+++ app/controllers/budget_groups_controller.rb
@@ -5,6 +5,16 @@ class BudgetGroupsController < ApplicationController
 
   def new
     @budget_group = BudgetGroup.new
+    @budget_groups = current_family.budget_groups
+  end
+
+  def create
+    @budget_group = current_family.budget_groups.new(budget_group_params)
+    if @budget_group.save
+      redirect_to new_budget_group_path, notice: "#{@budget_group.title}を作成しました"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -12,4 +22,11 @@ class BudgetGroupsController < ApplicationController
 
   def show
   end
+
+  private
+
+  def budget_group_params
+    params.require(:budget_group).permit(:title, :color)
+  end
+
 end
--- app/models/budget_group.rb
+++ app/models/budget_group.rb
@@ -5,4 +5,13 @@ class BudgetGroup < ApplicationRecord
 
   validates :title, presence: { message: "項目名は必須です"}, length: {maximum: 8, message: "項目名は8文字以内でお願いします"}
 
+  COLORS = [
+    ["白", "white"],
+    ["赤", "#fd2e36"],
+    ["オレンジ", "#ff7927"],
+    ["黄色", "#f6c344"],
+    ["緑", "#a0db35"],
+    ["水色", "#1688fa"],
+    ["青", "#414fcd"],
+  ]
 end
